4feeb29180364d2c76910a17ff8583dab3c8e1f7
Heitor Chang <heitorchang@gmail.com>
2016-07-09 19:54:46 -0300

    load js file

---
+++ pensejs/content/js/sinal.js
@@ -0,0 +1,9 @@
+function sinal(x) {
+  if (x < 0) {
+    return "Negativo";
+  } else if (x > 0) {
+    return "Positivo";
+  } else {
+    return "Zero";
+  }
+}
--- pensejs/content/o-primeiro-programa.lisp
+++ pensejs/content/o-primeiro-programa.lisp
@@ -1,5 +1,19 @@
 (:section
  (:p "Conteudo")
 
+ (:pre :data-language "javascript"
+       "function eGrande(x) {
+  if (x > 9000) {
+    console.log('É grande!!');
+  } else {
+    console.log('Não muito grande.')
+  }
+}")
+
+ (:p "
+Testar sinal de um numero")
+ 
+ (:pre :data-language "javascript"
+       (:print (load-file-as-string "js/sinal.js")))
 
  ) ;; END section
