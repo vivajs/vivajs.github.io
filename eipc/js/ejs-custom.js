a68ad99324720e8436c2e21d91561fae859dfa94
Heitor Chang <heitorchang@gmail.com>
2016-07-11 16:23:43 -0300

    eipc template

--- eipc/js/ejs-custom.js
+++ eipc/js/ejs-custom.js
@@ -1,21 +1,9 @@
 var myCustomJS = {};
 
-myCustomJS['box1'] = "\n\
-function box1(x) { \n\
-  return x + 10; \n\
-} \n\
-";
+myCustomJS['templateA'] = `
+function templateA(x) { return x + 100; }
+`;
 
-myCustomJS['box2'] = "\n\
-function box2(x) { \n\
-  return -x; \n\
-} \n\
-";
-
-myCustomJS['templateA'] = "\n\
-function templateA(x) { return x + 1000; }\n\
-";
-
-myCustomJS['templateB'] = "\n\
-function templateB(x) { return 3 * x; }\n\
-";
+myCustomJS['templateB'] = `
+function templateB(x) { return x * 10; }
+`;
